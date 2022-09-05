bd7b9bf90f453564ac262b441d08b9440e6b22e7
Miyayu <yuchan2215@gmail.com>
2022-09-05 18:47:11 +0900

    add: HTTPレスポンスコードを列挙するクラスを作成

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/constant/HTTPResponseCode.kt
@@ -0,0 +1,9 @@
+package jp.co.yumemi.android.code_check.constant
+
+object HTTPResponseCode {
+    const val OK = 200
+    const val NOT_MODIFIED = 304
+    const val VALIDATION_FAILED = 422
+    const val SERVICE_UNAVAILABLE = 503
+    const val FORBIDDEN = 403
+}

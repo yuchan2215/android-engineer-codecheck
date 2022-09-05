7ba43ae40585e1cb774ae26edd25cd9118628a9c
Miyayu <yuchan2215@gmail.com>
2022-09-05 14:02:53 +0900

    delete: 不要なnon-nullを削除

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragmentViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchFragmentViewModel.kt
@@ -29,7 +29,7 @@ class SearchFragmentViewModel : ViewModel() {
         val client = HttpClient(Android)
 
         return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client?.get("https://api.github.com/search/repositories") {
+            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
                 header("Accept", "application/vnd.github.v3+json")
                 parameter("q", inputText)
             }!!

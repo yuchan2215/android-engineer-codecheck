6610a2e22e471cc68b85f994a94dfe59b8375e55
Miyayu <yuchan2215@gmail.com>
2022-09-05 20:25:04 +0900

    add: エラーの説明をエラーモデルに持たせた

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/model/status/ErrorStatus.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/model/status/ErrorStatus.kt
@@ -1,30 +1,51 @@
 package jp.co.yumemi.android.code_check.model.status
 
+import androidx.annotation.StringRes
+import java.net.UnknownHostException
+import jp.co.yumemi.android.code_check.CodeCheckApplication
+import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.constant.HTTPResponseCode
 import okhttp3.ResponseBody
 import retrofit2.Response
 
 /**エラーステータス*/
-sealed class ErrorStatus {
+sealed class ErrorStatus(val errorDescription: String) {
 
     /**[Throwable]を所持する[ErrorStatus]*/
-    data class ThrowableError(val throwable: Throwable) : ErrorStatus()
+    data class ThrowableError(val throwable: Throwable) : ErrorStatus(
+        if (throwable is UnknownHostException)
+            getString(R.string.unknown_host_exception_error_description)
+        else
+            throwable.toString()
+    )
 
     // HTTPResponseCodeにて定義されたHTTPエラー一覧
     /**拒否エラー*/
-    object ForbiddenError : ErrorStatus()
+    object ForbiddenError : ErrorStatus(
+        getString(R.string.forbidden_error_description)
+    )
 
     /**バリデーションエラー*/
-    object ValidationError : ErrorStatus()
+    object ValidationError : ErrorStatus(
+        getString(R.string.validation_error_description)
+    )
 
     /**サービスが提供されないエラー*/
-    object ServiceUnavailableError : ErrorStatus()
+    object ServiceUnavailableError : ErrorStatus(
+        getString(R.string.service_unavailable_error_description)
+    )
 
     /**データが更新されていないことを示すエラー*/
-    object NotModifiedError : ErrorStatus()
+    object NotModifiedError : ErrorStatus(
+        getString(R.string.not_modified_error_description)
+    )
 
     /**その他のエラー*/
-    data class OtherResponseError(val errorBody: ResponseBody?, val errorCode: Int) : ErrorStatus()
+    data class OtherResponseError(val errorBody: ResponseBody?, val errorCode: Int) : ErrorStatus(
+        "${errorBody?.toString()}\n" +
+            getString(R.string.response_code_prefix) +
+            errorCode
+    )
 
     companion object {
 

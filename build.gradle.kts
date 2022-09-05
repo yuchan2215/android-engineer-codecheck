a3b10a6343172b47e1b45d003561c3b96d78c3b7
Miyayu <yuchan2215@gmail.com>
2022-09-05 12:57:22 +0900

    add: Kotlinx-serializationを導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,9 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.1")
 
+        //Kotlin Serialization
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.10")
+
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }

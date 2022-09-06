193c714a1043446c93c6a3cb0871101aadabb9e2
Miyayu <yuchan2215@gmail.com>
2022-09-06 13:36:39 +0900

    add: SearchBottomSheetでViewModelを取得できるように

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/view/bottomsheet/SearchBottomSheet.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/view/bottomsheet/SearchBottomSheet.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import jp.co.yumemi.android.code_check.databinding.SearchBottomSheetBinding
+import jp.co.yumemi.android.code_check.viewmodel.SearchFragmentViewModel
 
 class SearchBottomSheet : BottomSheetDialogFragment() {
     companion object {
         const val TAG = "SEARCH_BOTTOM_SHEET"
     }
+
+    private val viewModel: SearchFragmentViewModel by viewModels({ requireParentFragment() })
 
     private var _binding: SearchBottomSheetBinding? = null
     private val binding get() = _binding!!

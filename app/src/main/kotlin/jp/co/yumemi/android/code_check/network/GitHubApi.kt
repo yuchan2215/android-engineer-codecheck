16b87a87c9c698012cfe4cfe7ec3f9eb77c745de
Miyayu <yuchan2215@gmail.com>
2022-09-10 13:35:01 +0900

    change: GitHubApiServiceは、認証情報を持つように修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/network/GitHubApi.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/network/GitHubApi.kt
@@ -3,6 +3,7 @@ package jp.co.yumemi.android.code_check.network
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -12,7 +13,7 @@ object GitHubApi {
     private const val BASE_URL = "https://api.github.com/"
 
     @OptIn(ExperimentalSerializationApi::class)
-    private val retrofit = Retrofit.Builder().apply {
+    private fun getRetrofit(token: String?) = Retrofit.Builder().apply {
         val parser = Json {
             ignoreUnknownKeys = true // 定義していないキーがあるため、無視する
         }
@@ -22,8 +23,11 @@ object GitHubApi {
         val logger = HttpLoggingInterceptor()
             .apply { level = HttpLoggingInterceptor.Level.BODY }
 
+        val headerAuthorizationInterceptor = getAuthorizationInterceptor(token)
+
         val client = OkHttpClient.Builder()
             .addInterceptor(logger)
+            .addInterceptor(headerAuthorizationInterceptor)
             .build()
 
         client(client)
@@ -31,7 +35,23 @@ object GitHubApi {
         baseUrl(BASE_URL)
     }.build()
 
-    val gitHubApiService: GitHubApiService by lazy {
-        retrofit.create(GitHubApiService::class.java)
+    /**
+     * GitHubの認証情報を持った[Interceptor]。
+     * [token]がNullまたは空であればなにもしない。
+     */
+    private fun getAuthorizationInterceptor(token: String?): Interceptor = Interceptor { chain ->
+        val request = chain.request()
+        if (token.isNullOrEmpty()) {
+            return@Interceptor chain.proceed(request)
+        }
+        val headers =
+            request.headers.newBuilder().add("Authorization", "Bearer $token")
+                .build()
+        val newRequest = request.newBuilder().headers(headers).build()
+        return@Interceptor chain.proceed(newRequest)
+    }
+
+    fun getGitHubApiService(token: String?): GitHubApiService {
+        return getRetrofit(token).create(GitHubApiService::class.java)
     }
 }

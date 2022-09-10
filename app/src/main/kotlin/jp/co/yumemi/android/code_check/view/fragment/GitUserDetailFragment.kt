070ff0f16ce442f4a5f2a60ac9821d6358a9a0c5
Miyayu <yuchan2215@gmail.com>
2022-09-10 18:16:27 +0900

    add: フラグメントのAppBar対応

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/view/fragment/GitUserDetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/view/fragment/GitUserDetailFragment.kt
@@ -1,10 +1,16 @@
 package jp.co.yumemi.android.code_check.view.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.databinding.GitUserDetailFragmentBinding
 import jp.co.yumemi.android.code_check.viewmodel.UserDetailViewModel
@@ -16,12 +22,44 @@ class GitUserDetailFragment : Fragment(R.layout.git_user_detail_fragment) {
         UserDetailViewModel.Companion.Factory(args.userName)
     }
 
+    /**
+     * メニューのリスナー。
+     * ブラウザで開くボタンとシェアボタンが用意されている。
+     */
+    private val listener = Toolbar.OnMenuItemClickListener { menuItem ->
+        val url = viewModel.loadedUser.value?.url
+        when (menuItem.itemId) {
+            R.id.open_browser -> {
+                if (url == null) return@OnMenuItemClickListener false
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            }
+            R.id.share -> {
+                if (url == null) return@OnMenuItemClickListener false
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, url)
+                }
+                startActivity(intent)
+            }
+            else -> return@OnMenuItemClickListener false
+        }
+        return@OnMenuItemClickListener true
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = GitUserDetailFragmentBinding.bind(view)
+
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
         binding.apply {
             this@apply.viewModel = this@GitUserDetailFragment.viewModel
             lifecycleOwner = viewLifecycleOwner
+            toolBar.setupWithNavController(navController, appBarConfiguration)
+            toolBar.setOnMenuItemClickListener(listener)
         }
         viewModel.fetchStatus()
     }
